025af5ae98fd173d34a4364082d7013f40731295
angelospaulo <angelospaulo@gmail.com>
2023-04-22 16:16:10 -0300

    timao

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas, time!");
+        System.out.println("Boas vindas, timão!");
     }
 }
